a4d38f87be8d059162e22103d03551488caefbd9
Mateusz Czapliński <czapkofan@gmail.com>
2025-03-24 17:44:21 +0100

    try01: attempt very simplified code

--- try01/b.bat
+++ try01/b.bat
@@ -3,6 +3,7 @@ setlocal
 :: build
 set PATH=C:\users\Mateusz\AppData\local\Arduino15\packages\sandeepmistry\tools\gcc-arm-none-eabi\5_2-2015q4\bin;%PATH%
 
+:: del try.o try.elf try.hex
 arm-none-eabi-as -g -mcpu=cortex-m0 -mthumb try.s -o try.o
 arm-none-eabi-ld try.o -T ./try.ld -o try.elf
 arm-none-eabi-objcopy -O ihex try.elf try.hex
