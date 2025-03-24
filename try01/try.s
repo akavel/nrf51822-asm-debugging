733cd85ddc64db319caf820638a49d8d86bd9ec1
Mateusz Czapliński <czapkofan@gmail.com>
2025-03-24 17:36:23 +0100

    try01: fix to compile on cortex-m0

--- try01/try.s
+++ try01/try.s
@@ -27,7 +27,7 @@ reset_handler:
 	ldr r0, =_dstart
 	ldr r1, =_dend
 
-	sub r2,r1,r0
+	subs r2,r1,r0
 
 	ldr r1, =_flash_dstart
 
@@ -35,20 +35,20 @@ cpy_loop:
 	ldrb r3, [r1]
 	strb r3, [r0]
 
-	add r1, r1, #1
-	add r0, r0, #1
+	adds r1, r1, #1
+	adds r0, r0, #1
 
-	sub r2, r2, #1
+	subs r2, r2, #1
 	cmp r2, #0
 	bne cpy_loop
 
 
 main:
-	ldr r9, =apa
-	ldr r9, [r9]
+	ldr r4, =apa
+	ldr r4, [r4]
 
-	ldr r7, =0xF00DF00D
-	ldr r8, =0x1337BEEF
+	ldr r5, =0xF00DF00D
+	ldr r6, =0x1337BEEF
 
 done:
 	b done
