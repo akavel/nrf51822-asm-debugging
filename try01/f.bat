1a793e57d45e3fe58dfde47a5006668ca6288c31
Mateusz Czapliński <czapkofan@gmail.com>
2025-03-24 17:51:36 +0100

    try01: drop spurious halt

--- try01/f.bat
+++ try01/f.bat
@@ -4,5 +4,5 @@ setlocal
 set PATH=C:\dnload\embedded-etc\openocd-v0.12.0\bin;%PATH%
 
 :: TODO: add "nrf51 mass_erase"
-openocd -f openocd.cfg -c "init" -c "halt" -c "program try.hex verify reset exit"
+openocd -f openocd.cfg -c "init" -c "program try.hex verify reset exit"
 
