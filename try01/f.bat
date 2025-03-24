7c464e0a7ba2c79c00dd569ee5ecdd48f2f698c0
Mateusz Czapliński <czapkofan@gmail.com>
2025-03-24 17:05:53 +0100

    try01: add PATH

--- try01/f.bat
+++ try01/f.bat
@@ -1,5 +1,7 @@
 @echo off
+setlocal
 :: flash
+set PATH=C:\users\Mateusz\AppData\local\Arduino15\packages\sandeepmistry\tools\gcc-arm-none-eabi\5_2-2015q4\bin;%PATH%
 
 openocd -f openocd.cfg -c "init" -c "program try.hex verify reset exit"
 
