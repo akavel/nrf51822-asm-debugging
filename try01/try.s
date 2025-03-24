50f0a49a3b01d184f5da1b9601acc7b6e613b008
Mateusz Czapliński <czapkofan@gmail.com>
2025-03-24 20:48:18 +0100

    try01: bring back cpy_loop, actually needed

--- try01/try.s
+++ try01/try.s
@@ -24,23 +24,23 @@ reset_handler:
 	ldr r0, =_estack
 	mov sp, r0
 
-	// ldr r0, =_dstart
-	// ldr r1, =_dend
+	ldr r0, =_dstart
+	ldr r1, =_dend
 
-	// subs r2,r1,r0
+	subs r2,r1,r0
 
-	// ldr r1, =_flash_dstart
+	ldr r1, =_flash_dstart
 
-// cpy_loop:
-	// ldrb r3, [r1]
-	// strb r3, [r0]
+cpy_loop:
+	ldrb r3, [r1]
+	strb r3, [r0]
 
-	// adds r1, r1, #1
-	// adds r0, r0, #1
+	adds r1, r1, #1
+	adds r0, r0, #1
 
-	// subs r2, r2, #1
-	// cmp r2, #0
-	// bne cpy_loop
+	subs r2, r2, #1
+	cmp r2, #0
+	bne cpy_loop
 
 
 main:
