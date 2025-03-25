f54fcfb47d9a26766f56c8480a205b6b3163c9a1
Mateusz Czapliński <czapkofan@gmail.com>
2025-03-26 00:04:24 +0100

    try02: initial objdump helper script

---
+++ try02/d.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+:: disassemble
+set PATH=C:\users\Mateusz\AppData\local\Arduino15\packages\sandeepmistry\tools\gcc-arm-none-eabi\5_2-2015q4\bin;%PATH%
+
+arm-none-eabi-objdump -s -d target\thumbv6m-none-eabi\release\try02
+
